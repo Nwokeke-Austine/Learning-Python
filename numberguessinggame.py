a162ff994dbad8babe4fbf7e0793711c15f99e11
Nwokeke Austine <nwokekeaustine2007@gmail.com>
2026-02-11 02:19:22 +0100

    Number guessing game completed

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -3,3 +3,19 @@
 #Count attempts
 #Print attempts when user wins
 
+secret_number = 7
+attempt = 0
+
+while True:
+    number = int(input("Guess the secret number: ")) #it is int(input) and not input(int)
+    attempt = attempt + 1
+    
+    if secret_number == number:
+        print("You guessed right")
+        print("You used", attempt, "attempts" )
+        break
+
+    else:
+        print("You guessed wrong, Try again")
+        attempt = attempt + 1
+        
