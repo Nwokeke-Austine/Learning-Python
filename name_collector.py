4161bef16a50bab11c51cccfdc81b48616ff0639
Nwokeke Austine <nwokekeaustine2007@gmail.com>
2026-02-12 02:32:24 +0100

    Daily practice

---
+++ name_collector.py
@@ -0,0 +1,9 @@
+#Student name collector
+stundets = []
+
+print("Enter the name of 5 students")
+for i in range(1,6):
+    name = input("Name: ")
+    stundets.append(name)
+
+print(stundets)
---
+++ pass_or_fail.py
@@ -0,0 +1,7 @@
+scores = [45, 67, 89, 23, 56]
+
+for score in scores:
+    if score >= 50:
+        print(score, "PASS")
+    else:
+        print(score, "FAIL")
