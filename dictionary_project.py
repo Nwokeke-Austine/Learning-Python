14e49cd3257d6137450a9d9e34db069391e8f648
Nwokeke Austine <nwokekeaustine2007@gmail.com>
2026-02-17 23:30:30 +0100

    Daily Practice

--- dictionary_project.py
+++ dictionary_project.py
@@ -1,13 +1,18 @@
 #program to keep accepting input from user based on the number of students the user wants to input; name, level, department, age, sex. And store it
 
+students = []
 number_of_students = int(input("How many students: "))
 
+
 for i in range(number_of_students):
-    print("student", i+1)
+    student = {}
 
-    student = input("Enter name of student: ")
+    print("Student", i+1)
+    student["name"] = input("Enter Student's Name: ")
+    student["department"] = input("Enter Student's Department: ")
+    student["regno"] = input("Enter Registration Number: ")
 
-print (student)
+    students.append(student)
 
-student_name = { }
-
+for s in students:
+    print(s)
