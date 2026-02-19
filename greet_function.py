9ca9fb10843638e0f9b449b0e8f27e56621306ad
Nwokeke Austine <nwokekeaustine2007@gmail.com>
2026-02-19 11:55:57 +0100

    Daily practice

--- greet_function.py
+++ greet_function.py
@@ -6,8 +6,28 @@
 
 # greet("Austine")
 
-def add(a, b):
-    return(a + b)
+# def add(a, b):
+#     return(a + b)
 
-x = add(5, 3)
-print(x)
+# x = add(5, 3)
+# print(x)
+def student_info(name, regno, department):
+    print("-------------------------------------")
+    print("Name:",name )
+    print("Registration Number:", regno)
+    print("Department:", department)
+
+# student = []
+
+# number_of_students = int(input("How many Students: "))
+
+# for i in range(number_of_students):
+#     students = {}
+#     print(f"Student{i+1}")
+    student_info(input("Enter your name: "), int(input("Enter your age: ")) , input("Enter your department: ") )
+
+    # student.append(student_info)
+
+
+
+
