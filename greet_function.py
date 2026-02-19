059d0a8fdf3c2218f1d0db5b7e3fff3898bc67d1
Nwokeke Austine <nwokekeaustine2007@gmail.com>
2026-02-19 09:53:04 +0100

    Daily practice

--- greet_function.py
+++ greet_function.py
@@ -1,4 +1,13 @@
-def greet():
-    print("hello world")
+# def greet():
+#     print("hello world")
 
-greet()
+# def greet(name):
+#     status = input(f"Hello {name} how're you doing today? ")
+
+# greet("Austine")
+
+def add(a, b):
+    return(a + b)
+
+x = add(5, 3)
+print(x)
