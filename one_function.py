2c8b11df3cf0da491d1897fe55ea0bcb33e3b2b9
Nwokeke Austine <nwokekeaustine2007@gmail.com>
2026-02-26 23:27:45 +0100

    Daily practice

---
+++ one_function.py
@@ -0,0 +1,63 @@
+scores = []
+num_of_scores = int(input("How many scores do you want to enter: "))
+
+
+for i in range(num_of_scores):
+    enter_score = int(input(f"Enter score {i+1}: "))
+    scores.append(enter_score)
+
+
+
+def find_min_max_avg_total(scores):
+    highest = scores[0]
+    lowest = scores[0]
+    total = 0
+
+    #highest
+    for score in scores:
+        if score > highest:
+            highest = score
+
+    #lowest
+    for score in scores:
+        if score < lowest:
+            lowest = score
+
+    #Average
+    for score in scores:
+        total = score + total
+    
+    average = total/len(scores)
+
+    #Total
+    for score in scores:
+        total = score + total
+
+    return highest, lowest, average, total
+
+Max_score, Min_score, average_score, total_score = find_min_max_avg_total()
+print("The Max Score is", Max_score)
+print("The Min Score is", Min_score)
+print("The Average Score is", average_score)
+print("The Total Score is", total_score)
+
+
+#CORRECT VERSION
+# def find_min_max_avg_total(scores):
+#     highest = scores[0]
+#     lowest = scores[0]
+#     total = 0
+
+#     for score in scores:
+#         if score > highest:
+#             highest = score
+
+#         if score < lowest:
+#             lowest = score
+
+#         total += score
+
+#     average = total / len(scores)
+
+#     return highest, lowest, average, total
+
