3cef85740fc8c1e93f357bc8f25b890926199358
Nwokeke Austine <nwokekeaustine2007@gmail.com>
2026-03-11 15:06:26 +0100

    Daily Practice

--- student record project/student_record.py
+++ student record project/student_record.py
@@ -9,22 +9,38 @@ num_of_students = int(input("Enter number of students you want to register: "))
 
 students_list = []
 
-
+#grade function
+def calculate_grade(score):
+    if score > 70:
+        return("A")
+    elif score > 60:
+        return("B")
+    elif score > 50:
+        return("C")
+    elif score > 45:
+        return("D")
+    elif score > 40:
+        return("E")
+    else:
+        return("F")
+    
 for i in range(num_of_students):
     print("-----------------------------------------")
-    print(f"------ Student {1 + i} ------")
+    print(f"------ STUDENT {1 + i} ------")
     name = input("Enter name of student: ")
     department = input("Enter department of student: ")
     regno = input("Enter registration number: ")
-    level = input("Enter Level: ")
+    level = int(input("Enter Level: "))
     score = int(input("Student Score: "))
+    grade = calculate_grade(score)
 
     student = {
     "Name" : name,
     "Department" : department,
     "Registration Number" : regno,
     "Level" : level,
-    "Score" : score    
+    "Score" : score, 
+    "Grade" : grade
 }
     
     students_list.append(student)
@@ -38,3 +54,5 @@ for student in students_list:
     print("Registration Number:", student["Registration Number"])
     print("Level:", student["Level"])
     print("Score:", student["Score"])
+    print("Grade:", student["Grade"])
+
