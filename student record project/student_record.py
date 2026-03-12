9a968ac76a5596d14bf8024e05cc7e72e07cd992
Nwokeke Austine <nwokekeaustine2007@gmail.com>
2026-03-12 23:17:42 +0100

    Daily Forgr challenge

--- student record project/student_record.py
+++ student record project/student_record.py
@@ -55,12 +55,23 @@ for student in students_list:
     print("---------------------------")
     for key, value in student.items():
         print(f"{key}: {value}")
-    # print("---------------------------")
-    # print("Name:", student["Name"])
-    # print("Department:", student["Department"])
-    # print("Registration Number:", student["Registration Number"])
-    # print("Level:", student["Level"])
-    # print("Score:", student["Score"])
-    # print("Grade:", student["Grade"])
 
-# highest_score = 
+highest_score = students_list[0]["Score"]
+lowest_score = students_list[0]["Score"]
+total_score = 0
+
+for student in students_list:
+    score = student["Score"]
+    total_score += score
+    if score > highest_score:
+        highest_score = score
+    if score < lowest_score:
+        lowest_score = score
+
+average_score = total_score/len(students_list)
+
+print("\n-----STATISTICS-----")
+print(f"Higest Score: {highest_score}")
+print(f"Lowest Score: {lowest_score}")
+print(f"Class Average: {average_score}")
+        
